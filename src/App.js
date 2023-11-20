69157a0f2e07cf44e067a4b3a28a24917b7e54e2
jaydenset <jaydenset@gmail.com>
2023-11-19 18:35:11 -0800

    jayden quiz.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './app.css';
 import { HomePage } from './homepage';
 import { ReviewPage } from './review';
 import { SignUpPage } from './sign-up';
+import { QuizPage } from './quiz';
 
 
 
@@ -12,7 +13,7 @@ function App() {
         <div>
             <Routes>
                 <Route path='/' element={<HomePage />} />
-                {/* <Route path='/quiz' element={<QuizPage />} /> */}
+                <Route path='/quiz' element={<QuizPage />} />
                 <Route path='/review' element={<ReviewPage />} />
                 {/* <Route path='/mood' element={<MoodPage />} />
                 <Route path='/eg_mood' element={<EgMoodPage />} /> */}
---
+++ src/quiz.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export function QuizPage() {
+  return (
+    <div className="nav-container">
+      <span className="menu material-icons">menu</span>
+      <header>FOR ALL THE MOODS</header>
+      <span className="profile material-icons">account_circle</span>
+
+      <nav>
+        <ul>
+            <li><Link to='/'>HOME PAGE</Link></li>
+            <li><Link to='/quiz'>QUIZ</Link></li>
+            <li><Link to='/review'>REVIEW</Link></li>
+            <li><Link to='/mood'>MOOD</Link></li>
+            <li><Link to='/eg_mood'>MOOD EXAMPLE</Link></li>
+            <li><Link to='/sign-up'>SIGN-UP</Link></li>
+        </ul>
+      </nav>
+
+      <main>
+        <div className="review-container">
+          <h1 className="quiz-title">WHAT TYPE OF DRAKE FAN ARE YOU?</h1>
+          <h1 className="question-number">Question 1/10</h1>
+
+          <section className="image-container">
+            <img className="drake-album2" src="img/Take-Care.jpeg" alt="Take Care Album Cover" />
+            <span className="right material-icons">chevron_right</span>
+          </section>
+          <p className="quiz-caption">What is your favorite song from Take Care?</p>
+
+          <section className="quiz-container">
+            <div className="flex-container-quiz">
+              <div className="question1">
+                <input type="checkbox" value="check" id="check1" />
+                <label htmlFor="check1">Take Care</label>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+
+      <footer>
+        <small>
+          &copy; 2023 For All the Moods
+        </small>
+      </footer>
+    </div>
+  );
+}
+
+  
