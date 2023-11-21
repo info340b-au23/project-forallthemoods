0b76a57ce0a0e2669fd9579fc7d79f0437a4a522
krxxsten <krrxsten@uw.edu>
2023-11-21 12:58:31 -0800

    kris

--- src/mood.js
+++ src/mood.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export function MoodPage()  {
     const [selectedMood, setSelectedMood] = useState(null);
@@ -29,17 +29,6 @@ export function MoodPage()  {
                 <span className="menu material-icons">menu</span>
                 <header>FOR ALL THE MOODS</header>
                 <span className="profile material-icons">account_circle</span>
-
-                <nav>
-                    <ul>
-                        <li><Link to="/">HOME PAGE</Link></li>
-                        <li><Link to="/quiz">QUIZ</Link></li>
-                        <li><Link to="/review">REVIEW</Link></li>
-                        <li><Link to="/mood">MOOD</Link></li>
-                        <li><Link to="/eg_mood">MOOD EXAMPLE</Link></li>
-                        <li><Link to="/signup">SIGN-UP</Link></li>
-                    </ul>
-                </nav>
             </div>
 
             <div className={`selector-container ${showModal ? 'modal-container' : ''}`}>
