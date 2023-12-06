3a86f7f5423a9887685dc21850b8ac1161f2757e
krxxsten <krrxsten@uw.edu>
2023-12-06 15:34:58 -0800

    fixing navbar

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+export function Header() {
+    return (
+        <div>
+            <div className="nav-container">
+                <span className="menu material-icons">menu</span>
+                <header>FOR ALL THE MOODS</header>
+                <span className="profile material-icons">account_circle</span>
+            </div>
+        </div>
+    );
+}
