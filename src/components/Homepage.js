f0bf02e608d79fcce270b9df11ece65c50b4f7f2
Hritika Singh <hsingh17@uw.edu>
2023-12-10 21:08:36 -0800

    tika - changes to homepage bootstrap

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,32 +7,24 @@ export function HomePage() {
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
 
-  const handleMoodChange = (e) => {
-    setSelectedMood(e.target.value);
+  const handleMoodChange = (selectedValue) => {
+    setSelectedMood(selectedValue);
+    navigate(`/${selectedValue}`);
+    handleCloseModal();
   };
 
   const handleShowModal = () => {
-    setShowModal(!showModal);
+    setShowModal(true);
   };
 
   const handleCloseModal = () => {
     setShowModal(false);
   };
 
-  const handleSubmit = () => {
-    if (selectedMood && ['party', 'nostalgic', 'heartbreak', 'club', 'hyped', 'combination'].includes(selectedMood)) {
-      navigate(`/${selectedMood}`);
-    } else {
-      console.error('Invalid or null selected mood');
-    }
-  };
-
   return (
     <div>
       <main>
         <div className={`selector-container ${showModal ? 'modal-container' : ''}`}>
-          
-
           <button className="mood-button" onClick={handleShowModal}>
             CHOOSE YOUR MOOD
           </button>
@@ -41,29 +33,20 @@ export function HomePage() {
             <div className="modal">
               <h1 className="select-title">SELECT YOUR MOOD</h1>
               <p className="select-notice">SELECT YOUR MOOD AND LISTEN TO A PLAYLIST CURATED FOR YOUR MOOD.</p>
-              <Dropdown as={ButtonGroup}>
+              <Dropdown as={ButtonGroup} onSelect={handleMoodChange}>
                 <Dropdown.Toggle variant="primary" className="dropdown-basic">
                   SELECT
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                  <Dropdown.Item onSelect={() => handleMoodChange('party')}>Party Drake</Dropdown.Item>
-                  <Dropdown.Item onSelect={() => handleMoodChange('nostalgic')}>Nostalgic Drake</Dropdown.Item>
-                  <Dropdown.Item onSelect={() => handleMoodChange('heartbreak')}>Heartbreak Drake</Dropdown.Item>
-                  <Dropdown.Item onSelect={() => handleMoodChange('club')}>Hit the Club Drake</Dropdown.Item>
-                  <Dropdown.Item onSelect={() => handleMoodChange('hyped')}>Hyped Drake</Dropdown.Item>
-                  <Dropdown.Item onSelect={() => handleMoodChange('combination')}>Combination Drake</Dropdown.Item>
+                  <Dropdown.Item eventKey="party">Party Drake</Dropdown.Item>
+                  <Dropdown.Item eventKey="nostalgic">Nostalgic Drake</Dropdown.Item>
+                  <Dropdown.Item eventKey="heartbreak">Heartbreak Drake</Dropdown.Item>
+                  <Dropdown.Item eventKey="club">Hit the Club Drake</Dropdown.Item>
+                  <Dropdown.Item eventKey="hyped">Hyped Drake</Dropdown.Item>
+                  <Dropdown.Item eventKey="combination">Combination Drake</Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
-
-              <div className="button-container">
-                <button className="submit-button" onClick={handleSubmit}>
-                  Submit
-                </button>
-                <button className="close-button" onClick={handleCloseModal}>
-                  Close
-                </button>
-              </div>
             </div>
           )}
         </div>
@@ -72,12 +55,12 @@ export function HomePage() {
           MERCH
         </a>
         <button className="quiz-button">
-          <Link to='/quiz' className="button-link">
+          <Link to="/quiz" className="button-link">
             QUIZ
           </Link>
         </button>
         <button className="review-button">
-          <Link to='/review' className="button-link">
+          <Link to="/review" className="button-link">
             REVIEW/RATE
           </Link>
         </button>
