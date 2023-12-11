fddc780225c4f6efea6214fe5e098bff34872ffb
jstinle <jxle@uw.edu>
2023-12-10 23:23:48 -0800

    bruh

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ export function NaviBar() {
       <div className="nav-container">
         <header>FOR ALL THE MOODS</header>
         <span className="profile material-icons" onClick={handleProfileClick}>account_circle</span>
-      <Navbar expand="lg" className="nav-containers">
+      <Navbar expand="lg" className="nav-container">
         <Container className='me-auto'>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="nav">
