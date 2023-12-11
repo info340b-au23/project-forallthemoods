273c4827c3b44acf8c660579a2c51217222544ce
Hritika Singh <hsingh17@uw.edu>
2023-12-10 20:54:15 -0800

    tika - changes bootstrap

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { Dropdown, ButtonGroup } from 'react-bootstrap';
 
 export function HomePage() {
   const [selectedMood, setSelectedMood] = useState(null);
@@ -30,62 +31,56 @@ export function HomePage() {
     <div>
       <main>
         <div className={`selector-container ${showModal ? 'modal-container' : ''}`}>
-          <button className="btn btn-primary mood-button" onClick={handleShowModal}>
+          
+
+          <button className="mood-button" onClick={handleShowModal}>
             CHOOSE YOUR MOOD
           </button>
 
           {showModal && (
-            <div className="modal fade show" role="dialog" style={{ display: 'block' }}>
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h1 className="modal-title">SELECT YOUR MOOD</h1>
-                  </div>
-                  <div className="modal-body">
-                    <p className="select-notice">SELECT YOUR MOOD AND LISTEN TO A PLAYLIST CURATED FOR YOUR MOOD.</p>
-                    <form className="selector">
-                      <div className="form-group">
-                        <select
-                          className="form-control"
-                          id="mood"
-                          name="mood"
-                          onChange={handleMoodChange}
-                          value={selectedMood || ''}
-                        >
-                          <option value="null">Choose a Drake</option>
-                          <option value="party">Party Drake</option>
-                          <option value="nostalgic">Nostalgic Drake</option>
-                          <option value="heartbreak">Heartbreak Drake</option>
-                          <option value="club">Hit the Club Drake</option>
-                          <option value="hyped">Hyped Drake</option>
-                          <option value="combination">Combination Drake</option>
-                        </select>
-                      </div>
-                    </form>
-                  </div>
-                  <div className="modal-footer">
-                    <button className="btn btn-primary" onClick={handleSubmit}>
-                      Submit
-                    </button>
-                    <button className="btn btn-secondary" onClick={handleCloseModal}>
-                      Close
-                    </button>
-                  </div>
-                </div>
+            <div className="modal">
+              <h1 className="select-title">SELECT YOUR MOOD</h1>
+              <p className="select-notice">SELECT YOUR MOOD AND LISTEN TO A PLAYLIST CURATED FOR YOUR MOOD.</p>
+              <Dropdown as={ButtonGroup}>
+                <Dropdown.Toggle variant="primary" className="dropdown-basic">
+                  SELECT
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item onSelect={() => handleMoodChange('party')}>Party Drake</Dropdown.Item>
+                  <Dropdown.Item onSelect={() => handleMoodChange('nostalgic')}>Nostalgic Drake</Dropdown.Item>
+                  <Dropdown.Item onSelect={() => handleMoodChange('heartbreak')}>Heartbreak Drake</Dropdown.Item>
+                  <Dropdown.Item onSelect={() => handleMoodChange('club')}>Hit the Club Drake</Dropdown.Item>
+                  <Dropdown.Item onSelect={() => handleMoodChange('hyped')}>Hyped Drake</Dropdown.Item>
+                  <Dropdown.Item onSelect={() => handleMoodChange('combination')}>Combination Drake</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <div className="button-container">
+                <button className="submit-button" onClick={handleSubmit}>
+                  Submit
+                </button>
+                <button className="close-button" onClick={handleCloseModal}>
+                  Close
+                </button>
               </div>
             </div>
           )}
         </div>
 
-        <a className="merch-button btn btn-primary button-link" href="https://us.octobersveryown.com/" target="_blank" rel="noopener noreferrer">
+        <a className="merch-button button-link" href="https://us.octobersveryown.com/" target="_blank" rel="noopener noreferrer">
           MERCH
         </a>
-        <Link to="/quiz" className="btn btn-primary quiz-button">
-          QUIZ
-        </Link>
-        <Link to="/review" className="btn btn-primary review-button">
-          REVIEW/RATE
-        </Link>
+        <button className="quiz-button">
+          <Link to='/quiz' className="button-link">
+            QUIZ
+          </Link>
+        </button>
+        <button className="review-button">
+          <Link to='/review' className="button-link">
+            REVIEW/RATE
+          </Link>
+        </button>
       </main>
     </div>
   );
